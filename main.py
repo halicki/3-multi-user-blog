f8da7c7253a916ac27490221d697b69f7c98bc71
Arkadiusz Halicki <halicki.arkadiusz@gmail.com>
2017-02-27 16:26:24 +0100

    some shit

--- main.py
+++ main.py
@@ -78,18 +78,20 @@ class Welcome(Handler):
 
 
 USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
-def valid_username(username):
-    return USER_RE.match(username)
-
-
-PASS_RE = re.compile(r"^.{3,20}$")
-def valid_password(password):
-    return PASS_RE.match(password)
-
-
 EMAIL_RE = re.compile(r"^[\S]+@[\S]+.[\S]+$")
+PASS_RE = re.compile(r"^.{3,20}$")
+
+
+def valid_username(username):
+    return username and USER_RE.match(username)
+
+
+def valid_password(password):
+    return password and PASS_RE.match(password)
+
+
 def valid_email(email):
-    return EMAIL_RE.match(email)
+    return email and EMAIL_RE.match(email)
 
 
 class SignUp(Handler):
@@ -121,12 +123,12 @@ class SignUp(Handler):
 
         if username_error or password_error or verify_error or email_error:
             self.render('signup.html',
-                username=username,
-                username_error=username_error,
-                password_error=password_error,
-                verify_error=verify_error,
-                email_error=email_error
-            )
+                        username=username,
+                        username_error=username_error,
+                        password_error=password_error,
+                        verify_error=verify_error,
+                        email_error=email_error
+                        )
         else:
             self.redirect('welcome?username={}'.format(username))
 
